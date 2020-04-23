a0de9f4349dd8f8c7e651097c0043436b522eaa3
hajime OGUSHI <info@m-g-n.me>
2020-04-24 01:02:53 +0900

    add mgn

--- frontity.settings.js
+++ frontity.settings.js
@@ -2,16 +2,23 @@ const settings = {
   "name": "my-app",
   "state": {
     "frontity": {
-      "url": "https://test.frontity.io",
-      "title": "Test Frontity Blog",
-      "description": "WordPress installation for Frontity development"
+      "url": "https://www.m-g-n.me/",
+      "title": "株式会社mgn",
+      "description": "WordPressでのWebサイトの制作"
     }
   },
   "packages": [
     {
       "name": "frontity-chakra-theme",
-      "state": {
-        "theme": {
+      state: {
+        theme: {
+          // The logo can be a text or an image url
+          logo: "mgn",
+          // show background pattern
+          showBackgroundPattern: true,
+          // show social links
+          showSocialLinks: true,
+		  // the top-level navigation labels and links
           "menu": [
             [
               "Home",
@@ -22,20 +29,49 @@ const settings = {
               "/about/"
             ],
             [
-              "AMP",
-              "/tag/amp/"
+              "Portfolio",
+              "/category/portfolio/"
             ],
             [
-              "Block Editor",
-              "/tag/block-editor/"
+              "News",
+              "/category/news/"
             ]
           ],
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
+          // the social links
+          socialLinks: [
+            ["pinterest", "https://www.pinterest.com/frontity/"],
+            ["facebook", "https://www.instagram.com/frontity/"],
+            ["twitter", "https://www.twitter.com/frontity/"]
+          ],
+          // color shades to use in the blog
+          colors: {
+            primary: {
+              "50": "#e9f5f2",
+              "100": "#d4dcd9",
+              "200": "#bbc3be",
+              "300": "#a1aba5",
+              "400": "#87938b",
+              "500": "#6d7972",
+              "600": "#555f58",
+              "700": "#323c34",
+              "800": "#232924",
+              "900": "#272727"
+            },
+            accent: {
+              "50": "#ede4d3",
+              "100": "#fbe3b2",
+              "200": "#f6d086",
+              "300": "#f1be58",
+              "400": "#eca419",
+              "500": "#d49212",
+              "600": "#a5710b",
+              "700": "#775105",
+              "800": "#483100",
+              "900": "#1d0f00"
+            }
           }
         }
-      }
+      },
     },
     {
       "name": "@frontity/wp-source",
